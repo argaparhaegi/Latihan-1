88d36c1b3b66ce6b74bd4f854f614cfe6a52638f
Arga <argaparhaegy02@gmail.com>
2019-03-19 18:45:46 +0700

    Tugas 1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUGAS_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class Mahasiswa 
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            // mengeset nilai properties objek saya dan kamu 
+            saya.Nim = "12345";
+            saya.Nama = "Reza";
+            saya.Ipk = 2;
+
+            kamu.Nim = "12345";
+            kamu.Nama = "anton";
+            kamu.Ipk = 3;
+
+            // memanggil methode registrasi dan isi Krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+              
+        }
+    }
+}
